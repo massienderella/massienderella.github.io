1af16f60142dce1d7430c012f5b6b650d5ca2788
Massienderella <massiel.ams@gmail.com>
2018-03-26 17:38:27 -0300

    transiciones + espacio entre proyectos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,15 @@
 $( document ).ready(function(){
   $(".button-collapse").sideNav();
 });
+
+$(".about").smoove({
+  offset  : '20%',
+});
+
+$(".portfolio").smoove({
+  offset  : '20%',
+});
+
+$("#skills-container").smoove({
+  offset  : '30%',
+});
