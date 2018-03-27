9934116f8439fe401e2ba2cf1ceeddcde04370d9
Massienderella <massiel.ams@gmail.com>
2018-03-27 00:20:54 -0300

    logo + button up

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,3 +13,22 @@ $(".portfolio").smoove({
 $("#skills-container").smoove({
   offset  : '30%',
 });
+
+$('.back-to-top').css({"display" : "none"});
+jQuery(document).ready(function() {
+var offset = 250; 
+var duration = 300; 
+jQuery(window).scroll(function() { 
+if (jQuery(this).scrollTop() > offset) {
+jQuery('.back-to-top').fadeIn(duration);
+} else { 
+jQuery('.back-to-top').fadeOut(duration); 
+} 
+});
+ 
+jQuery('.back-to-top').click(function(event) { 
+event.preventDefault(); 
+jQuery('html, body').animate({scrollTop: 0}, duration);
+return false; 
+}) 
+});
